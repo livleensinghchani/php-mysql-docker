54d8855338b341fdbd10c5dacd5c0022e72106d9
livleensinghchani <livleensinghchani@gmail.com>
2024-07-06 17:58:36 +0530

    Base File

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+//***************************************** */
+//  Landing Page for the server!
+
+//  This is the file that will be served 
+//  by the server by default!
+
+//  Default serving address is 8000
+//  Port 80
+
+//  Refer to //LINK - docker-compose.yml 
+//***************************************** */
+
+  echo"<h1>HEY USER</h1>";
